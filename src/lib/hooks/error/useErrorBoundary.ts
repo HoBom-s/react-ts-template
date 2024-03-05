96f3aa0f7ae0acc3cd9948faa551ef488b6e6b32
foxmon <jjockrod@naver.com>
2024-03-05 14:00:11 +0900

    kit

--- src/lib/hooks/error/useErrorBoundary.ts
+++ src/lib/hooks/error/useErrorBoundary.ts
@@ -1,5 +1,23 @@
 import { useState } from "react";
 
+/**
+ * 가장 가까운 ErrorBoundary로 Error를 throw 하도록 한다.
+ *
+ * @example
+ *      const throwError = useErrorBoundary();
+ *
+ *
+ *      const SomeComponent = () => {
+ *          // ...
+ *          return (
+ *              <button onClick={() => {
+ *                  throwError(new Error("Error를 부모에게 위임."));
+ *              }}>
+ *                  Error throw
+ *              </button>
+ *          );
+ *      }
+ */
 export const useErrorBoundary = <ErrorType extends Error>() => {
   const [error, setError] = useState<ErrorType | null>(null);
 
