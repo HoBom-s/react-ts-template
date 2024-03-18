401a849f7f650c458b7e7a93f141768edd59b2e8
foxmon <jjockrod@naver.com>
2024-03-18 15:16:16 +0900

    template

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { PublicRouter } from "./routes";
 
+const RETRY_COUNT: number = 0;
+const STALE_TIME: number = 1 * 60 * 1000;
+const GC_TIME: number = 5 * 60 * 1000;
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      retry: 0,
+      retry: RETRY_COUNT,
+      staleTime: STALE_TIME,
+      gcTime: GC_TIME,
+      refetchOnMount: true,
+      refetchOnWindowFocus: true,
     },
   },
 });
